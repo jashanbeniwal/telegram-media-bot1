aea16af68407d77a296c7186414efee3df49fbe2
jashanbeniwal <beniwaljass279@gmail.com>
2025-11-14 12:18:25 +0530

    Update telethon_client.py

--- utils/telethon_client.py
+++ utils/telethon_client.py
@@ -1,16 +1,25 @@
 from telethon import TelegramClient
 from config import Config
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 class TelethonClient:
     _instance = None
     
     def __init__(self):
-        self.client = TelegramClient(
-            "media_bot_session",
-            Config.API_ID,
-            Config.API_HASH
-        )
+        if Config.STRING_SESSION:
+            self.client = TelegramClient(
+                "media_bot_session",
+                Config.API_ID,
+                Config.API_HASH
+            )
+            self.enabled = True
+        else:
+            self.client = None
+            self.enabled = False
+            logger.warning("Telethon client disabled - STRING_SESSION not configured")
     
     @classmethod
     def get_instance(cls):
@@ -19,14 +28,18 @@ class TelethonClient:
         return cls._instance
     
     async def start(self):
-        if Config.STRING_SESSION:
+        if self.enabled and Config.STRING_SESSION:
             await self.client.start(session_string=Config.STRING_SESSION)
+            logger.info("Telethon client started successfully")
         else:
-            await self.client.start(bot_token=Config.BOT_TOKEN)
+            logger.warning("Telethon client not started - STRING_SESSION not configured")
     
     async def upload_large_file(self, chat_id: int, file_path: str, caption: str = "", 
                               thumb: str = None, progress_callback=None):
         """Upload large files using Telethon (for files > 2GB)"""
+        if not self.enabled:
+            raise Exception("Telethon client not configured. Set STRING_SESSION environment variable.")
+        
         try:
             file = await self.client.upload_file(
                 file_path,

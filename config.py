512ff69c8218f1b41807ca06efed650db4630e43
jashanbeniwal <beniwaljass279@gmail.com>
2025-11-14 12:10:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ load_dotenv()
 
 class Config:
     # Bot Configuration
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = int(os.getenv("API_ID", 0))
-    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID", "25331263"))
+    API_HASH = os.getenv("API_HASH", "cab85305bf85125a2ac053210bcd1030")
     
     # Telethon String Session (for large file uploads)
     STRING_SESSION = os.getenv("STRING_SESSION")
     
     # MongoDB Configuration
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-    DATABASE_NAME = os.getenv("DATABASE_NAME", "telegram_media_bot")
+    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://rs92573993688:pVf4EeDuRi2o92ex@cluster0.9u29q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "rs92573993688")
     
     # Temporary Directory
     TEMP_DIR = os.getenv("TEMP_DIR", "temp")
@@ -23,7 +23,7 @@ class Config:
     MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4GB
     
     # Admin User IDs
-    ADMINS = list(map(int, os.getenv("ADMINS", "").split())) if os.getenv("ADMINS") else []
+    ADMINS = list(map(int, os.getenv("ADMINS", "1955406483").split())) if os.getenv("ADMINS") else []
     
     # Thumbnail Configuration
     THUMBNAIL_SIZE = (320, 320)
